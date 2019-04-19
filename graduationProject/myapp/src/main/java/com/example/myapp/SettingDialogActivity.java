581f5a49390a41c0a56d226bd14c1a46838af220
gamjapark <gojang4@gmail.com>
2019-04-19 15:42:48 +0900

    feat(Setting): complete delete device function

--- graduationProject/myapp/src/main/java/com/example/myapp/SettingDialogActivity.java
+++ graduationProject/myapp/src/main/java/com/example/myapp/SettingDialogActivity.java
@@ -62,7 +62,7 @@ public class SettingDialogActivity extends Activity {
         switchFinishBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (pushSwitch.isChecked() == true) {
+                if (pushSwitch.isChecked()) {
 
                     if (originValue == 1) {
 
@@ -81,10 +81,7 @@ public class SettingDialogActivity extends Activity {
 
                     if (originValue == 1) {
 
-                        /*
-                            추가되어있을 경우 단말 제거 기능 요청
-                         */
-                        Toast.makeText(getApplicationContext(), "Push 알림 OFF", Toast.LENGTH_SHORT).show();
+                        DeleteDeviceHandler();
 
                     } else {
 
@@ -194,6 +191,97 @@ public class SettingDialogActivity extends Activity {
             e.printStackTrace();
         }
     }
+
+    /*
+        DeleteDeviceHandler: void
+        = 단말 제거 요청 처리 핸들러
+    */
+
+    public void DeleteDeviceHandler(){
+        RequestInfo requestInfo = new RequestInfo(RequestInfo.RequestType.DELETE_DEVICE);
+
+        String url = "http://" + requestInfo.GetRequestIP() + ":" + requestInfo.GetRequestPORT() + requestInfo.GetProcessURL();
+
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String response){
+                        DeleteDeviceResponse(response);
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error){
+                        error.printStackTrace();
+                    }
+                }
+        ){
+            @Override
+            protected Map<String, String> getParams(){
+                return DeleteDeviceRequest();
+            }
+        };
+        request.setShouldCache(false);
+        Volley.newRequestQueue(getApplicationContext()).add(request);
+        Log.d("요청 url: ", url);
+    }
+
+    /*
+        DeleteDeviceRequest(): Map<String, String>
+        = 단말 제거 요청 전달 파라미터 설정 함수
+    */
+
+    private Map<String, String> DeleteDeviceRequest(){
+        Map<String, String> params = new HashMap<>();
+
+        params.put("mobile", myDeviceInfo.getMobile());
+
+        return params;
+    }
+
+    /*
+        DeleteDeviceResponse(String): void
+        = 단말 제거 요청 응답 처리 함수
+    */
+
+    private void DeleteDeviceResponse(String response){
+        try{
+            Log.d("onResponse 호출 ", response);
+
+            JSONObject json = new JSONObject(response);
+            String resultString = (String) json.get("message");
+
+            switch (resultString) {
+                case "success":
+                    ShowToast("Push 알림 OFF");
+
+                    Intent intent = new Intent();
+                    intent.putExtra("DeviceCheckResult", "NO");
+                    setResult(RESULT_OK, intent);
+
+                    finish();
+                    break;
+
+                case "fail":
+                    ShowToast("Push 알림 OFF 실패");
+                    pushSwitch.setChecked(true);
+                    break;
+
+                case "error":
+                    ShowToast("단말 추가  중 오류 발생");
+                    break;
+
+                case "db_fail":
+                    ShowToast("연결 오류");
+                    break;
+
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
     private void ShowToast(String s){
         Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
