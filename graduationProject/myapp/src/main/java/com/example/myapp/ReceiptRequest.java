6f93a1d3ba76ed711a4a9087aa20ca12f0c18213
gamjapark <gojang4@gmail.com>
2019-05-15 02:03:02 +0900

    feat(receipt):complete add receipt func

--- graduationProject/myapp/src/main/java/com/example/myapp/ReceiptRequest.java
+++ graduationProject/myapp/src/main/java/com/example/myapp/ReceiptRequest.java
@@ -14,10 +14,16 @@ import java.util.Map;
 
 public class ReceiptRequest {
 
+    private String hDate;
+    private String hValue;
     private String hName;
+    private String userID;
+    private String cId;
     private RequestInfo.RequestType rType;
     private Context context;
 
+
+    //상점명 확인 요청시
     ReceiptRequest(String hName, RequestInfo.RequestType rType, Context context){
 
         this.hName = hName;
@@ -26,8 +32,21 @@ public class ReceiptRequest {
 
     }
 
+    //영수증 내역 추가 요청시
+    ReceiptRequest(String hDate, String hValue, String hName, String userID, String cId, RequestInfo.RequestType rType, Context context){
+
+        this.hDate = hDate;
+        this.hValue = hValue;
+        this.hName = hName;
+        this.userID = userID;
+        this.cId = cId;
+        this.rType = rType;
+        this.context = context;
+
+    }
+
     public interface VolleyCallback{
-        void onSuccess(String cId);
+        void onSuccess(String id);
     }
 
     public void StoreRequest(final VolleyCallback callback){
@@ -94,6 +113,80 @@ public class ReceiptRequest {
             e.printStackTrace();
         }
     }
+
+    public void AddReceipt(final VolleyCallback callback){
+        RequestInfo requestInfo = new RequestInfo(rType);
+
+        String url = "http://" + requestInfo.GetRequestIP() + ":" + requestInfo.GetRequestPORT() + requestInfo.GetProcessURL();
+
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                response -> AddReceiptResponse(response, callback),
+                error -> { error.getMessage(); error.printStackTrace(); })
+        {
+            @Override
+            protected Map<String, String> getParams(){
+                return AddReceiptRequest();
+            }
+        };
+        request.setShouldCache(false);
+        Volley.newRequestQueue(context).add(request);
+        Log.d("요청 url: ", url);
+
+    }
+
+     /*
+        AddReceiptRequest(): Map<String, String>
+        = 영수증 추가 요청 전달 파라미터 설정 함수
+    */
+
+    private Map<String, String> AddReceiptRequest(){
+
+        Map<String, String> params = new HashMap<>();
+
+        params.put("hDate", hDate);
+        params.put("hValue", hValue);
+        params.put("hName", hName);
+        params.put("id", userID);
+        params.put("cId", cId);
+
+        return params;
+
+    }
+
+    /*
+        AddReceiptResponse(String): void
+        = 영수증 추가 요청 응답 처리 함수
+    */
+
+    private void AddReceiptResponse(String response, final VolleyCallback callback){
+        try{
+            Log.d("onResponse 호출 ", response);
+
+            JSONObject json = new JSONObject(response);
+            String resultString = (String) json.get("message");
+
+            switch (resultString) {
+                case "success":
+                    String hId = (String) json.get("data");
+                    callback.onSuccess(hId);
+                    break;
+
+                case "fail":
+                    break;
+
+                case "error":
+                    break;
+
+                case "db_fail":
+                    break;
+
+            }
+
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
 
 
 }
